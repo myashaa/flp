6ad5986b20470df02aa69f00e3153f7806fd6902
myashaa <79213041+myashaa@users.noreply.github.com>
2024-01-28 17:16:12 +0300

    Update task3.hs

--- Haskell/ТЕМА3/task3.hs
+++ Haskell/ТЕМА3/task3.hs
@@ -1,1 +1,19 @@
 -- Определите функцию myunion, которая находит объединение двух списков. Объединением двух списков будет список содержащий элементы, которые есть по крайней мере в одном из списков
+
+import Data.List (nub)
+
+union :: [a] -> [a] -> [a] 
+union [] ys = ys 
+union xs [] = xs 
+union (x:xs) (y:ys) = x : y : union xs ys
+
+removeDuplicates :: Eq a => [a] -> [a] 
+removeDuplicates = nub
+
+myunion :: Eq a => [a] -> [a] -> [a]
+myunion l1 l2 = removeDuplicates (union l1 l2)
+
+main = do
+  let l1 = [1,2,3,3,4]
+  let l2 = [4,5,6]
+  print(myunion l1 l2)
