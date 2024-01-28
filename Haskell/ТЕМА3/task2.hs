7382cd2809c95b2218a8f94305469d06835973f8
myashaa <79213041+myashaa@users.noreply.github.com>
2024-01-28 15:50:03 +0300

    Update task2.hs

--- Haskell/ТЕМА3/task2.hs
+++ Haskell/ТЕМА3/task2.hs
@@ -1,1 +1,10 @@
 -- Определите функцию secondlastlist. Эта функция берет список списков и возвращает последние элементы каждого, объединенные  в список
+
+secondlastlist :: [[a]] -> [a]
+secondlastlist [] = []
+secondlastlist (x:xs) | length x == 0 = secondlastlist xs
+                      | otherwise = last x : secondlastlist xs
+
+main = do
+  let l = [[1], [], [4,5,6]] 
+  print(secondlastlist l)
