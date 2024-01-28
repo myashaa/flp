20b8d830357eef2da1eff2c01bc538de2b416943
myashaa <79213041+myashaa@users.noreply.github.com>
2024-01-28 15:27:46 +0300

    Update task1.hs

--- Haskell/ТЕМА3/task1.hs
+++ Haskell/ТЕМА3/task1.hs
@@ -1,1 +1,10 @@
 -- Определите функцию listnums. Она берет численный аргумент n и возвращает список всех чисел от n до 1, включительно
+
+listnums :: Int -> [Int]
+listnums n | n <= 0 = []
+           | n == 1 = [1]
+           | otherwise = n : listnums (n - 1)
+
+main = do
+  let n = 10 
+  print(listnums n)
