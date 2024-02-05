d38e10ca0eac43e8c3e6de2f72d38419ea5b8e8c
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-05 16:19:32 +0300

    Create task1.hs

---
+++ Haskell/ТЕМА4/task1.hs
@@ -0,0 +1,21 @@
+-- splitAt takes a number and a list. It then splits the list at that many elements, returning the resulting two lists in a tuple.
+
+len :: [a] -> Int
+len [] = 0
+len (_:xs) = 1 + len xs
+
+mySplitAt :: Int -> [a] -> ([a], [a])
+mySplitAt n xs
+  | n < 0 = error "invalid index"
+  | n == 0 = ([], xs)
+  | n > length xs = (xs, [])
+  | otherwise = (take n xs, drop n xs)
+
+main = do
+  print(splitAt 2 [1,2,3,4,5])
+  print(splitAt 2 [1])
+  print(splitAt 10 [1,2,3,4,5])
+  
+  print(mySplitAt 2 [1,2,3,4,5])
+  print(mySplitAt 2 [1])
+  print(mySplitAt 10 [1,2,3,4,5])
