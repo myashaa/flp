86b8e4112439fd6440762b86492b5cc90596c15a
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-08 20:58:45 +0300

    Update task2.hs

--- Haskell/ТЕМА5/task2.hs
+++ Haskell/ТЕМА5/task2.hs
@@ -1,1 +1,20 @@
--- 2
+-- Скопировать информацию из одного файла в другой, заменив знаки  пунктуации заданным с клавиатуры  символом. Имена файлов указываются в командной строке.
+
+import System.IO
+import Data.Char
+
+replacePunctuation :: Char -> String -> String
+replacePunctuation replaceChar = map (\c -> if isPunctuation c then replaceChar else c)
+
+main :: IO()
+main = do
+  hSetBuffering stdout NoBuffering
+  putStr "Введите название входного файла:"
+  inputFile <- getLine
+  putStr "Введите название выходного файла:"
+  outputFile <- getLine
+  putStr "Введите символ, котрый будет использоваться для замены:"
+  replaceChar <- getChar
+
+  inputText <- readFile inputFile
+  writeFile outputFile (replacePunctuation replaceChar inputText)
