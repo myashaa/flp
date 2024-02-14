878b238a8198bd4301cf3599b55e93eb7b658e90
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-14 08:27:14 +0300

    Create task2.hs

---
+++ Haskell/КОНТРОЛЬНАЯ РАБОТА/task2.hs
@@ -0,0 +1,10 @@
+-- Напишите функцию elemIndices :: Eq a => a -> [a] -> [Int], которая находит, под какими индексами в списке встречается заданный элемент
+
+import Data.List (findIndices)
+
+elemIndices :: Eq a => a -> [a] -> [Int]
+elemIndices x xs = findIndices (==x) xs
+
+main = do
+  print(elemIndices 'a' "banana")
+  print(elemIndices 1 [2,1,3,1,4,1])
