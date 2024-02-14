ca8577089a4d16b1a8a2444432b9d548ac3f9b66
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-14 08:53:22 +0300

    Update task2.hs

--- Haskell/КОНТРОЛЬНАЯ РАБОТА/task2.hs
+++ Haskell/КОНТРОЛЬНАЯ РАБОТА/task2.hs
@@ -7,4 +7,6 @@ elemIndices x xs = findIndices (==x) xs
 
 main = do
   print(elemIndices 'a' "banana")
+  print(elemIndices 't' "banana")
   print(elemIndices 1 [2,1,3,1,4,1])
+  print(elemIndices 7 [2,1,3,1,4,1])
