b2282297d1b245594c28b3a4ce0c2377b17e1605
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-14 09:05:29 +0300

    Update task1.hs

--- Haskell/КОНТРОЛЬНАЯ РАБОТА/task1.hs
+++ Haskell/КОНТРОЛЬНАЯ РАБОТА/task1.hs
@@ -1,1 +1,19 @@
+-- Напишите функцию partitionN :: [a] -> Int ->[[a]], которая разбивает заданный список на указанное количество подсписков. N-1 подсписков должны иметь одинаковую длину,последний элемент списка списков может содержать меньшее количество элементов
 
+partitionN :: [a] -> Int -> [[a]]
+partitionN xs n
+  | n <= 0 = error "Количество подсписков должно быть положительным числом"
+  | otherwise = split xs n
+
+split :: [a] -> Int -> [[a]]
+split [] _ = []
+split xs n = take m xs : split (drop m xs) (n - 1)
+  where m = (length xs + n) `div` n
+
+main = do
+print(partitionN [1,2,3,4,5,6,7] 3)
+print(partitionN [1,2,3,4,5,6,7] 4)
+print(partitionN [1] 3)
+print(partitionN [1,2,3,4,5,6,7] 1)
+print(partitionN [1,2,3,4,5,6,7] 0)
+                
