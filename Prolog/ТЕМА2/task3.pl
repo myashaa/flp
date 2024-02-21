955232b4eb7d6321eb2077a9a60d16c6c82465a6
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-21 09:56:25 +0300

    Create task3.pl

---
+++ Prolog/ТЕМА2/task3.pl
@@ -0,0 +1,9 @@
+seg(1, point(1, 11), point(14, 11)).
+seg(2, point(2, 4), point(13, 4)).
+seg(3, point(2, 2), point(9, 2)).
+seg(4, point(3, 10), point(3, 1)).
+seg(5, point(7, 10), point(13, 10)).
+seg(6, point(8, 13), point(8, 0)).
+seg(7, point(10, 12), point(10, 3)).
+seg(8, point(11, 13), point(11, 3)).
+seg(9, point(12, 12), point(12, 2)).
