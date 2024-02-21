e85c19a6c0f813b2ec755380d23f7baf4f2cddd5
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-21 10:10:47 +0300

    Update task1.pl

--- Prolog/ТЕМА1/task1.pl
+++ Prolog/ТЕМА1/task1.pl
@@ -20,19 +20,24 @@ parent(jim, bob).
 parent(ann, bob).
 
 % Вопросы
+% ?- Составить вопрос и найти в базе данных бабушку для bob
 % ?- grandmother(X, bob).
 grandmother(X, Y) :- female(X), parent(X, Z), parent(Z, Y).
 
+% ?- Составить вопрос и найти в базе данных внука
 % ?- grandson(X, Y).
 grandson(X, Y) :- male(X), parent(Y, Z), parent(Z, X).
 
+% ?- Составить вопрос и найти в базе данных сестру для jim
 % ?- sister(X, jim).
 different(X, Y) :- X \= Y.
 sister(X, Y) :- female(X), parent(Z, X), parent(Z, Y), different(X, Y).
 
+% ?- Определите отношение "тётя"
 % ?- aunt(ann, tammy).
 aunt(X, Y) :- sister(X, Z), parent(Z, Y).
 
+% ?- Определите отношение "кузин"
 % ?- cousin(bob, tammy).
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), different(X, Y).
 cousin(X, Y) :- parent(A, X), parent(B, Y), sibling(A, B).
