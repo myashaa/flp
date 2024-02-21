19b80fa4f54d703eba358a99db6fd660712200d3
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-21 10:12:17 +0300

    Update task2.pl

--- Prolog/ТЕМА1/task2.pl
+++ Prolog/ТЕМА1/task2.pl
@@ -10,11 +10,13 @@ likes(eric, chess).
 likes(paul, swimming).
 
 % Вопросы
+% ?- Составить вопрос и найти тех, кто имеет четыре хобби
 % ?- has_four_hobbies(john).
 has_four_hobbies(A) :- likes(A, X1), likes(A, X2), likes(A, X3), likes(A, X4),
   different(X1, X2), different(X1, X3), different(X1, X4),
   different(X2, X3), different(X2, X4),
   different(X3, X4).
-  
+
+% ?- Составить вопрос и найти тех, у кого одинаковые хобби
 % ?- have_shared_hobby(ellen, john, reading).
 have_shared_hobby(A, B, X) :- likes(A, X), likes(B, X), different(A, B).
