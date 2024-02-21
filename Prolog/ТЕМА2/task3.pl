035d472b52c9df15305c9888749c72c4bba32bf6
myashaa <79213041+myashaa@users.noreply.github.com>
2024-02-21 09:59:15 +0300

    Update task3.pl

--- Prolog/ТЕМА2/task3.pl
+++ Prolog/ТЕМА2/task3.pl
@@ -7,3 +7,17 @@ seg(6, point(8, 13), point(8, 0)).
 seg(7, point(10, 12), point(10, 3)).
 seg(8, point(11, 13), point(11, 3)).
 seg(9, point(12, 12), point(12, 2)).
+
+% Вопросы
+% ?- perimetr(A,B,C,D,P,S).
+per_sq(A, B, C, D, P, S) :- 
+  cross(A, B, point(X1, Y1), _, _),
+  cross(A, D, point(X2, Y1), _, _),
+  cross(C, B, point(X1, Y2), _, _),
+  cross(C, D, point(X2, Y2), _, _),
+  Y1 > Y2,
+  X1 > X2,
+  S1 is abs(X2 - X1),
+  S2 is abs(Y1 - Y2),
+  P is (S1 + S2) * 2,
+  S is S1 * S2.
